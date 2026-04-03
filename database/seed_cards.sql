b34ea32a76052b2610509b70f28200777e743c50
Aidan McGohan <aidanmcgohan@gmail.com>
2026-04-02 23:31:01 -0400

    added reksai to database seed

--- database/seed_cards.sql
+++ database/seed_cards.sql
@@ -1,3 +1,9 @@
+-- Maha from MTG
 INSERT INTO cards (card_img, game, set, cn, treatment, card_name)
 VALUE
 ('https://cards.scryfall.io/large/front/c/f/cf3320ec-c4e8-405a-982d-e009c58c9e21.jpg?1721426449', 'mtg', 'BLB', 0100, 'Standard', 'Maha, Its Feathers Night');
+
+-- Rek'Sai from Riftbound
+INSERT INTO cards (card_img, game, set, cn, treatment, card_name)
+VALUES
+('https://tcgplayer-cdn.tcgplayer.com/product/666721_in_1000x1000.jpg', 'riftbound', 'SFD', 029, 'alternate', 'Reksai - Breacher (Alterante Art)');
