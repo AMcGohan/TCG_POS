63ecc92a7cae0eb14649102781cdede694ddc600
Aidan McGohan <aidanmcgohan@gmail.com>
2026-04-02 23:31:31 -0400

    added riftbound to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Mtg } from './mtg/mtg';
+import { Riftbound } from './riftbound/riftbound';
 
 export const routes: Routes = [
     { path: 'mtg', component: Mtg},
+    { path: 'rift', component: Riftbound},
 ];
