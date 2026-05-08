d9d947feb52038fcbcfd2b63b34deb9d1c81a1a3
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-08 16:59:39 -0400

    setting up for future tcgs

--- backend/src/routes/cards.routes.js
+++ backend/src/routes/cards.routes.js
@@ -4,6 +4,11 @@ import { listMTGCards, listRiftCards } from "../controllers/cards.controllers.js
 const router = Router();
 
 router.get("/mtg/:name", listMTGCards);
-router.get("/rift", listRiftCards);
+
+// router.get("/rift/:name", listRiftCards);
+// router.get("/op/:name", listOPCards);
+// router.get("/swu/:name", listSWUCards);
+// router.get("/fab/:name", listFABCards);
+// router.get("/pkmn/:name", listPKMNCards);
 
 export default router;
