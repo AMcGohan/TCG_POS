e65497a34ba59ecfa8d2f866579e84e35ec68fe3
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-08 16:57:43 -0400

    Added admin routes for creating and viewing employees

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cardRoutes from "./routes/cards.routes.js"
 import listRoutes from "./routes/lists.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 export function createApp() {
 
@@ -14,6 +15,7 @@ export function createApp() {
 
     app.use("/api/cards", cardRoutes);
     app.use("/api/list", listRoutes);
+    app.use("/api/admin", adminRoutes);
     
     return app;
 }
