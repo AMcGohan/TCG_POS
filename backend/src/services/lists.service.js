8fcb1abaf9423220db3029cb63f426d930d78271
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-08 17:00:11 -0400

    added addToBuyListService

--- backend/src/services/lists.service.js
+++ backend/src/services/lists.service.js
@@ -1,3 +1,4 @@
+import { addToBuyList } from "../controllers/lists.controllers.js";
 import * as listModels from "../models/lists.models.js";
 
 export async function getBuyListService(id) {
@@ -6,4 +7,8 @@ export async function getBuyListService(id) {
 
 export async function createBuyListService(buyListData) {
     return await listModels.createBuyListService(buyListData);
+}
+
+export async function addToBuyListService(id, cardsToAdd) {
+    return await listModels.addToBuyListService(id, cardsToAdd);
 }
