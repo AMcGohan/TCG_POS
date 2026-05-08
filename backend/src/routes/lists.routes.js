889674ac1d044622b6b25be86f4aaf428a96efeb
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-08 16:59:51 -0400

    added cards to add to list API call

--- backend/src/routes/lists.routes.js
+++ backend/src/routes/lists.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { fetchBuyList, createBuyList } from "../controllers/lists.controllers.js";
+import { fetchBuyList, createBuyList, addToBuyList } from "../controllers/lists.controllers.js";
 
 const router = Router();
 
@@ -8,5 +8,9 @@ router.get("/:id", fetchBuyList)
 
 // Create lists
 router.post("/", createBuyList)
+
+// Insert card into list 
+// TODO fetch price_id based on card_id and foil bool
+router.post("/:id", addToBuyList)
 
 export default router;
