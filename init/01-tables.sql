027b392378eceb4c9b4fc240604021245ded87d8
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-09 01:12:10 -0400

    Fixed foreign key references.

--- init/01-tables.sql
+++ init/01-tables.sql
@@ -8,22 +8,6 @@ CREATE TABLE `card` (
   `treatment` VARCHAR(255)
 );
 
-CREATE TABLE `buy_list` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `customer_id` INT,
-  `emp_id` INT,
-  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `card_order` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `list_id` INT,
-  `card_id` INT,
-  `price_id` INT,
-  `quantity` INT
-);
-
 CREATE TABLE `customer` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `fname` VARCHAR(255),
@@ -32,28 +16,25 @@ CREATE TABLE `customer` (
   `email` VARCHAR(255)
 );
 
-CREATE TABLE `trade_list` (
+CREATE TABLE `employee` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `emp_fname` VARCHAR(255),
+  `emp_lname` VARCHAR(255)
+);
+
+CREATE TABLE `buy_list` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `customer_id` INT,
-  `status` enum('in progress','awaiting response','finished'),
-  `credit` decimal(10,2),
+  FOREIGN KEY (customer_id) REFERENCES customer(id),
   `emp_id` INT,
   `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `card_trade` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `list_id` INT,
-  `card_id` INT,
-  `price_id` INT,
-  `trade_value` decimal(10,2),
-  `quantity` int
-);
-
 CREATE TABLE `card_price` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `card_id` INT,
+  FOREIGN KEY (card_id) REFERENCES card(id),
   `reg_price` decimal(10,2),
   `foil_price` decimal(10, 2),
   `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -63,38 +44,44 @@ CREATE TABLE `card_price` (
 CREATE TABLE `card_inventory` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `card_id` INT,
+  FOREIGN KEY (card_id) REFERENCES card(id),
   `condition` ENUM('Damaged', 'Heavily Played', 'Moderately Played','Light Played'),
   `count` INT,
   `foil` boolean,
   `location` ENUM('Binder', 'Case', 'Box')
 );
 
-CREATE TABLE `employee` (
+CREATE TABLE `card_order` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `emp_fname` VARCHAR(255),
-  `emp_lname` VARCHAR(255)
+  `list_id` INT,
+  FOREIGN KEY (list_id) REFERENCES buy_list(id) ON DELETE CASCADE,
+  `card_id` INT,
+  FOREIGN KEY (card_id) REFERENCES card(id),
+  `price_id` INT,
+  FOREIGN KEY (price_id) REFERENCES card_price(id),
+  `quantity` INT
 );
 
-ALTER TABLE `buy_list` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+CREATE TABLE `trade_list` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `customer_id` INT,
+  FOREIGN KEY (customer_id) REFERENCES customer(id),
+  `status` ENUM('in progress','awaiting response','finished'),
+  `credit` decimal(10,2),
+  `emp_id` INT,
+  FOREIGN KEY (emp_id) REFERENCES employee(id),
+  `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
-ALTER TABLE `buy_list` ADD FOREIGN KEY (`emp_id`) REFERENCES `employee` (`id`);
-
-ALTER TABLE `card_order` ADD FOREIGN KEY (`list_id`) REFERENCES `buy_list` (`id`);
-
-ALTER TABLE `card_order` ADD FOREIGN KEY (`card_id`) REFERENCES `card` (`id`);
-
-ALTER TABLE `card_order` ADD FOREIGN KEY (`price_id`) REFERENCES `card_price` (`id`);
-
-ALTER TABLE `trade_list` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
-
-ALTER TABLE `trade_list` ADD FOREIGN KEY (`emp_id`) REFERENCES `employee` (`id`);
-
-ALTER TABLE `card_trade` ADD FOREIGN KEY (`list_id`) REFERENCES `trade_list` (`id`);
-
-ALTER TABLE `card_trade` ADD FOREIGN KEY (`card_id`) REFERENCES `card` (`id`);
-
-ALTER TABLE `card_trade` ADD FOREIGN KEY (`price_id`) REFERENCES `card_price` (`id`);
-
-ALTER TABLE `card_price` ADD FOREIGN KEY (`card_id`) REFERENCES `card` (`id`);
-
-ALTER TABLE `card_inventory` ADD FOREIGN KEY (`card_id`) REFERENCES `card` (`id`);
+CREATE TABLE `card_trade` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `list_id` INT,
+  FOREIGN KEY (list_id) REFERENCES trade_list(id) ON DELETE CASCADE,
+  `card_id` INT,
+  FOREIGN KEY (card_id) REFERENCES card(id),
+  `price_id` INT,
+  FOREIGN KEY (price_id) REFERENCES card_price(id),
+  `trade_value` decimal(10,2),
+  `quantity` int
+);
