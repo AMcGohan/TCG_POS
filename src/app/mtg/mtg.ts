28c1fbe98905ecb069749759686b9f14f1a5635c
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-13 15:14:09 -0400

    Imported card interace; Removed unnecessary comments

--- src/app/mtg/mtg.ts
+++ src/app/mtg/mtg.ts
@@ -1,13 +1,6 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-interface Card {
-  card_name: string;
-  card_img: string;
-  set: string;
-  cn: string;
-  treatment: string;
-}
+import { Card } from '../interfaces/card';
 
 @Component({
   selector: 'app-mtg',
@@ -18,7 +11,6 @@ interface Card {
 
 export class Mtg {
   MTG_API = "http://localhost:3000/api/cards/mtg";
-  SCRYFALL_API = "http://api.scryfall.com/cards/search?q=!"
 
   cards: Card[] = [];
 
@@ -46,47 +38,10 @@ export class Mtg {
       const result = await response.json();
 
       this.cards = result.cards;
-      // Fetch images and wait for completion
-      // await this.getMTGCardImages();
       this.cdr.detectChanges();
 
     } catch (error) {
       console.error('search failed', error);
     }
   }
-
-  /**
-   *  Called by search query; fetches images from Scryfall API and maps it to card by set and collector number
-   * @returns 
-   */
-// async getMTGCardImages() {
-//   try {
-
-//     const response = await fetch(this.SCRYFALL_API + `"${this.cards[0].card_name}"` + "&unique=prints");
-    
-//     if (!response.ok) {
-//       console.error("Image getter failed for", this.cards[0].card_name);
-//       return;
-//     }
-
-//     const result = await response.json();
-//     const imageIndex = new Map<string, string>();
-
-//     for (const print of result.data) {
-//       const key = `${print.set}-${print.collector_number}`;
-//       imageIndex.set(key.toLowerCase(), print.image_uris.small);
-//     }
-
-//     console.log("Image index: ", imageIndex);
-
-//     for (const card of this.cards) {
-//       const key = `${card.setCode}-${card.number}`;
-//       card.imgUrl = imageIndex.get(key.toLowerCase()) || '';
-//     }
-
-//     console.log("This.cards: ", this.cards);
-//   } catch (error) {
-//     console.error('image fetch failed ', error);
-//   }
-// }
 }
