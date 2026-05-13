3bbac68fec547f7fb9bac4fbb05593256d7f6fb8
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-13 16:19:48 -0400

    added list and card_order interfaces

---
+++ src/app/interfaces/card_order.ts
@@ -0,0 +1,7 @@
+export interface CardOrder {
+    id: number;
+    list_id: number;
+    card_id: number;
+    price_id: number;
+    quantity: number;
+}
---
+++ src/app/interfaces/list.ts
@@ -0,0 +1,7 @@
+export interface List{
+    id: number;
+    customer_id: number;
+    emp_id: number;
+    createdAt: string;
+    updatedAt: string;
+}
