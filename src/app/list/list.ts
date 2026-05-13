f0faac5931bdd1acc20ee117a22599d0c4c65dab
Aidan McGohan <aidanmcgohan@gmail.com>
2026-05-13 17:44:29 -0400

    Adds card to customer list; arguments are hardcoded at the moment

--- src/app/list/list.ts
+++ src/app/list/list.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { CardOrder } from '../interfaces/card_order';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ApiResponse, CardListItem } from '../interfaces';
 
 @Component({
   selector: 'app-list',
@@ -12,10 +12,17 @@ import { FormsModule } from '@angular/forms';
 export class List {
 
   // Contains array of cards associated with list_id, card_id, and price_id.
-  currentList: CardOrder[] = [];
+  // currentList: CardList[] = [];
 
   // API URL
   LIST_URL = "http://localhost:3000/api/list/";
+  currentList: CardListItem[] = [];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.fetchList(4);
+  }
 
 
   /**
@@ -39,8 +46,22 @@ export class List {
     return result.buyListData.id;
   } 
 
-  async addCard(card_id: number) {
+  async addCard(list_id: number, card_id: number, quantity: number) {
+    const response = await fetch(this.LIST_URL + list_id, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({"card_id": card_id, "price_id": 1, "quantity": quantity})
+    });
 
+    if (!response.ok) {
+      console.error("Add to List failed");
+      return
+    }
+    const result = await response.json();
+    console.log(result);
+    return result;
   }
 
   async fetchList(list_id: number) {
@@ -50,8 +71,9 @@ export class List {
       console.error("List Fetch failed");
     }
 
-    const result = await response.json();
-    console.log(result);
-    return result;
+    const data: ApiResponse = await response.json();
+    console.log(data);
+    this.cdr.detectChanges();
+    this.currentList = data.buyList.Card_Orders;
   } 
 }
