66dfda27344ec3471dd41158b5445fac6cffd4bb
Aidan McGohan <aidanmcgohan@gmail.com>
2026-07-10 16:31:14 -0400

    removed clientlist and total price from app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,11 @@
 import { Component, signal, effect } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { List } from "./list/list";
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, List],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('TCG_POS');
-
-  clientList: any;
-  totalPrice = signal(0);
-
 }
